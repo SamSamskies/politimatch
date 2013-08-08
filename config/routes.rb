4a1518b18f9fd240e5c2dd1e3cd2b93da7b1fb87
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-08-08 11:54:43 -0700

    Add landing page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Politimatch::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -39,7 +36,7 @@ Politimatch::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
