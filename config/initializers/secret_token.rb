285f8b261e8bc3eb86ca983cb026644945f2eb88
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-08-08 11:41:17 -0700

    Rails skeleton with postgres and no test unit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Politimatch::Application.config.secret_key_base = '2b12bf314abd6f6e4e926644705c0ded5ed8bb57ae43568bbff9b979141b61142b48b18bdbe446416df462aa3ae47eae10b81fc6e08cbd17f500742e5b3447b5'
