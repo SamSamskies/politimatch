7466a694ec5005b5e9d938996a18fa29a933c007
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-08-16 17:00:27 -0700

    Add first name and last name attributes to schema for legislators.

--- db/migrate/20130816203114_create_legislators.rb
+++ db/migrate/20130816203114_create_legislators.rb
@@ -1,6 +1,8 @@
 class CreateLegislators < ActiveRecord::Migration
   def change
     create_table :legislators do |t|
+      t.string :first_name
+      t.string :last_name
       t.string :party
       t.string :twitter
       t.string :website
