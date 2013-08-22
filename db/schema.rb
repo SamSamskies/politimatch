822daca88f8f58ee60a0ba44655e2939a87166ab
Steven Miyakawa SAM <samprofessional@gmail.com>
2013-08-21 19:38:23 -0700

    Updated schema.

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20130816203623) do
     t.string   "website"
     t.string   "district"
     t.string   "photo"
-    t.integer  "leg_id"
+    t.string   "leg_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
