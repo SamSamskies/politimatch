2da41d472801f3f3d8f62278e6efb188f7f0e844
Clara <clara@chefsurfing.com>
2013-10-02 11:53:40 -0700

    updates in index page

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130816203623) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "bills", force: true do |t|
     t.string   "name"
     t.string   "number"
